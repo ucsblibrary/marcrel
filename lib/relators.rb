80fd48c656ad5bad5d252f75804b4730956605fd
Alex Dunn <dunn.alex@gmail.com>
2017-07-05 10:00:17 -0700

    move to lib subdirectory, update namespace

--- lib/relators.rb
+++ lib/relators.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-module Metadata
-  MARCREL =
+module Marcrel
+  RELATORS =
     {
       actor:                                 RDF::Vocab::MARCRelators.act,
       adapter:                               RDF::Vocab::MARCRelators.adp,
