6b137a3c1416444e6f61b647e8f701831303b4d7
Alex Dunn <dunn.alex@gmail.com>
2017-07-07 10:40:55 -0700

    fix lib structure for gem requires

---
+++ lib/metadata-fields.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+require File.expand_path('../metadata-fields/marcrel.rb', __FILE__)
+require File.expand_path('../metadata-fields/csv.rb', __FILE__)
--- lib/metadata-fields/csv.rb
+++ lib/metadata-fields/csv.rb
@@ -7,7 +7,7 @@ require File.expand_path('../marcrel.rb', __FILE__)
 module Fields
   CSV = YAML.safe_load(
     ::ERB.new(
-      File.read(File.expand_path('../../config/csv_headers.yml.erb', __FILE__))
+      File.read(File.expand_path('../../../config/csv_headers.yml.erb', __FILE__))
     ).result
   )
 end
