0b5fcf6a52aaa2baeb4d5a4408d2672e1bc3e17d
Alex Dunn <dunn.alex@gmail.com>
2017-07-12 09:32:47 -0700

    import MODS resource map from ADRL

--- lib/metadata-fields.rb
+++ lib/metadata-fields.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
-require File.expand_path('../metadata-fields/marcrel.rb', __FILE__)
 require File.expand_path('../metadata-fields/csv.rb', __FILE__)
+require File.expand_path('../metadata-fields/marcrel.rb', __FILE__)
+require File.expand_path('../metadata-fields/mods.rb', __FILE__)
